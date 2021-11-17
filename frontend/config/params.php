6467327822b236c6ca9edf05cdb2e7f86be438f6
Garmayev <garmayev@yandex.ru>
2021-11-17 02:53:47 +0000

    test

---
+++ frontend/config/params.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+	'telegram' => [
+		"bot_id" => '2124338715:AAFEScQu0Ny-_AEdC4e9Ngdvx9O_hnbJVXc',
+//		'manager' => [443353023, /* 581330380 */]
+	],
+	'clickatell' => [
+		'apiKey' => 'ujFl-fAeTqOSYZ7rq0Kzaw==',
+		'apiId' => '0359d49dcdad4229bb31876afe89c064',
+	]
+];
