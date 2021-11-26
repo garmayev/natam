fec717768a7ecd743d47d1823cad0315fcde5eda
Garmayev <garmayev@yandex.ru>
2021-11-26 11:40:22 +0800

    Remove leaflet

--- frontend/modules/admin/views/default/index.php
+++ frontend/modules/admin/views/default/index.php
@@ -5,8 +5,8 @@
 
 use yii\web\View;
 $this->registerJsFile("//api-maps.yandex.ru/2.1/?apikey=0bb42c7c-0a9c-4df9-956a-20d4e56e2b6b&lang=ru_RU", ["depends" => \yii\web\JqueryAsset::class]);
-$this->registerCssFile("http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css", ["depends" => \yii\web\JqueryAsset::class]);
-$this->registerJsFile("http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js", ["depends" => \yii\web\JqueryAsset::class]);
+//$this->registerCssFile("http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css", ["depends" => \yii\web\JqueryAsset::class]);
+//$this->registerJsFile("http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js", ["depends" => \yii\web\JqueryAsset::class]);
 $this->registerJsFile("/js/scout-map.js", ["depends" => \yii\web\JqueryAsset::class]);
 ?>
 
