f610b82609cc42d2d9b171f490aed3d79b91aec2
Garmayev <garmayev@yandex.ru>
2021-12-24 13:59:28 +0800

    Fixes

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -135,89 +135,85 @@ foreach ($products as $product) {
 $this->registerCssFile("/css/style.css");
 
 
-
 ?>
 <section class="form" id="form">
     <div class="container-fluid">
         <div class="form_inner">
             <img src="img/journal.png" class="journal" alt="journal">
-            <?php $form = ActiveForm::begin(["id" => "create-order", "action" => ["/order/create"], "options" => ["class" => "form_inner"]]); ?>
-                <div class="form_content">
-	            <?php
-                // TODO: Добавление товара в форме
-                // TODO: Исключение товара из списка вариантов
-                    if (!empty($cartItems = $cart->getItems())) { ?>
+			<?php $form = ActiveForm::begin(["id" => "create-order", "action" => ["/order/create"], "options" => ["class" => "form_inner"]]); ?>
+            <div class="form_content">
+				<?php if (!empty($cartItems = $cart->getItems())) { ?>
                     <div id="product_list" class="order-block active form_block">
-			            <?php
-			            foreach ($cart->getItems() as $cartItem) {
-				            $item = $cartItem->getProduct();
-				            ?>
+						<?php
+						foreach ($cart->getItems() as $cartItem) {
+							$item = $cartItem->getProduct();
+							?>
                             <div class="form-group col-lg-6 col-md-6 col-xs-12">
-					            <?php
-					            echo Html::dropDownList(
-						            "Order[product][id][]",
-						            $cartItem->getId(),
-						            $items,
-						            ["class" => ["form-control"]]
-					            );
-					            ?>
+								<?php
+								echo Html::dropDownList(
+									"Order[product][id][]",
+									$cartItem->getId(),
+									$items,
+									["class" => ["form-control"]]
+								);
+								?>
                             </div>
                             <div class="form-group col-lg-6 col-md-6 col-xs-12">
-					            <?= Html::textInput("Order[product][count][]", $cartItem->getQuantity(), ["class" => "form-control"]); ?>
+								<?= Html::textInput("Order[product][count][]", $cartItem->getQuantity(), ["class" => "form-control"]); ?>
                             </div>
-				            <?
-			            }
-			            ?>
+							<?
+						}
+						?>
                         <a href="#" class="btn blue">Next</a>
                     </div>
                     <div id="client_info" class="order-block form_block">
                         <div class="form-group col-lg-12 ol-md-12 col-xs-12">
-				            <?= Html::textInput("Client[name]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Name")]); ?>
+							<?= Html::textInput("Client[name]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Name")]); ?>
                         </div>
                         <div class="form-group col-lg-12 ol-md-12 col-xs-12">
-				            <?= Html::textInput("Client[phone]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Phone")]); ?>
+							<?= Html::textInput("Client[phone]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Phone")]); ?>
                         </div>
                         <div class="form-group col-lg-12 ol-md-12 col-xs-12">
-				            <?= Html::textInput("Client[company]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Company")]); ?>
+							<?= Html::textInput("Client[company]", "", ["class" => "form-control", "placeholder" => Yii::t("app", "Company")]); ?>
                         </div>
                         <a href="#" class="btn blue">Next</a>
                     </div>
                     <div id="order_info" class="order-block form_block">
                         <div class="form-group col-lg-12 ol-md-12 col-xs-12">
-				            <?= \kartik\datetime\DateTimePicker::widget([
+							<?= \kartik\datetime\DateTimePicker::widget([
 
-					            'name' => 'Order[delivery_date]',
-					            'options' => [
-						            "id" => "order-delivery_date",
-						            "placeholder" => Yii::t("app", "Delivery Date"),
-					            ],
-					            "pluginOptions" => [
-						            'daysOfWeekDisabled' => [0, 6],
-						            'hoursDisabled' => '0,1,2,3,4,5,6,7,8,20,21,22,23',
-						            'minuteStep' => 30,
+								'name' => 'Order[delivery_date]',
+								'options' => [
+									"id" => "order-delivery_date",
+									"placeholder" => Yii::t("app", "Delivery Date"),
+								],
+								"pluginOptions" => [
+									'daysOfWeekDisabled' => [0, 6],
+									'hoursDisabled' => '0,1,2,3,4,5,6,7,8,20,21,22,23',
+									'minuteStep' => 30,
 //				    'minView' => 1,
-						            'startDate' => date("Y-m-d"),
-						            'autoclose' => true,
-					            ]
-				            ]) ?>
+									'startDate' => date("Y-m-d"),
+									'autoclose' => true,
+								]
+							]) ?>
                         </div>
                         <div class="form-group col-lg-12 ol-md-12 col-xs-12">
-				            <?= Html::textInput("Order[address]", "", ["id" => "order-address", "class" => "form-control", "placeholder" => Yii::t("app", "Address"), "style" => "margin-bottom: 15px;"]); ?>
+							<?= Html::textInput("Order[address]", "", ["id" => "order-address", "class" => "form-control", "placeholder" => Yii::t("app", "Address"), "style" => "margin-bottom: 15px;"]); ?>
                             <input type="hidden" name="Location[title]" id="location-title"">
                             <input type="hidden" name="Location[latitude]" id="location-latitude">
                             <input type="hidden" name="Location[longitude]" id="location-logintude">
-                            <?php
-                            // TODO: Разобраться с шириной контейнера карты
-                            ?>
+							<?php
+							// TODO: Разобраться с шириной контейнера карты
+							?>
                             <div id="map" style="height: 50vh; width: 100%"></div>
                         </div>
-			            <?= Html::submitButton("FINISH", ["class" => ["btn", "blue", "finish"]]) ?>
+						<?= Html::submitButton("FINISH", ["class" => ["btn", "blue", "finish"]]); ?>
                     </div>
-	            <?php } else { ?>
+				<?php } else { ?>
                     <label>Корзина пуста</label>
-                <?php } ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+				<?php } ?>
+            </div>
+			<?php ActiveForm::end(); ?>
         </div>
     </div>
 </section>
