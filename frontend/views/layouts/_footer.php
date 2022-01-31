024b4651b8f8638cdc72ff79a99cc87420b7bfdb
Garmayev <garmayev@yandex.ru>
2022-01-31 15:43:22 +0800

    Yandex.metrika

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -82,3 +82,18 @@ use yii\widgets\Menu;
 <div style="text-align: center; padding: 5px 0; background: #054670; color: #fff;">
     <span class="copyright">Created by <a href="http://amgexpert.com/">AMGCompany</a></span>
 </div>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(87339199, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/87339199" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
