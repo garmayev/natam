2774d141e86871722c3b0cf6a128411e41e444f6
Garmayev <garmayev@yandex.ru>
2022-02-03 17:41:03 +0800

    fixes in new order

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -103,6 +103,7 @@ $this->title = Yii::$app->name;
 									'type' => DateTimePicker::TYPE_INPUT,
 									'options' => [
 										'class' => 'form',
+                                        'autocomplete' => false,
 										'id' => 'order-delivery_date',
 										'placeholder' => Yii::t('app', 'Delivery Date')
 									],
