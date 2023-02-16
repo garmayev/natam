81c83b7397b780e2cf4435c0201753dfb9572685
garmayev <garmayev@yandex.ru>
2023-02-16 09:47:49 +0800

    Check user info from telegram game

---
+++ frontend/modules/api/controllers/DefaultController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace frontend\modules\api\controllers;
+
+class DefaultController
+{
+
+}
--- frontend/views/telegram/game.php
+++ frontend/views/telegram/game.php
@@ -19,21 +19,30 @@ use yii\web\View;
     <script src="//code.jquery.com/jquery-3.6.3.min.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", () => {
-            let tg = window.Telegram.WebApp;
-            tg.expand();
-            if (tg.initData) {
-                $(".log").append($(`<p>${tg.initData.user.id}</p>`));
-            } else {
-                $(".log").append($(`<p>${tg.initDataUnsafe.user.id}</p>`));
-            }
+            $.ajax({
+                url: "/api/default/option"
+            }).then(response => {
+                console.log(response);
+                let tg = window.Telegram.WebApp;
+                tg.expand();
+                if (tg.initData) {
+                    document.querySelector(".log").textContent += `<p>${tg.initData.user.id}</p>`;
+                } else {
+                    document.querySelector(".log").textContent += `<p>${tg.initDataUnsafe.user.id}</p>`;
+                }
+            })
         })
     </script>
     <link rel="stylesheet" href="/css/webapp.css">
 </head>
 <body>
-<div class="cart">
-    <h1>Приложение находится на стадии разработки. Приносим свои извинения!</h1>
+<div class="slide">
+    <input type="text" name="User[login]" placeholder="<?= Yii::t('user', 'Login') ?>"/>
+    <input type="password" name="User[password]" placeholder="<?= Yii::t('user', 'Password') ?>"/>
     <div class="log"></div>
+</div>
+<div class="slide">
+
 </div>
 </body>
 </html>
