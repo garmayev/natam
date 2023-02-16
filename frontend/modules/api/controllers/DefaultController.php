f21d604968940bf03b06a67f7a7c44181ab38469
garmayev <garmayev@yandex.ru>
2023-02-16 09:52:23 +0800

    Fix game

--- frontend/modules/api/controllers/DefaultController.php
+++ frontend/modules/api/controllers/DefaultController.php
@@ -2,7 +2,20 @@
 
 namespace frontend\modules\api\controllers;
 
-class DefaultController
-{
+use Yii;
+use yii\base\Controller;
+use yii\web\Response;
 
+class DefaultController extends Controller
+{
+    public function beforeAction($action)
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        return parent::beforeAction($action);
+    }
+
+    public function actionOption()
+    {
+        return [Yii::$app->getRequest()->csrfParam => Yii::$app->getRequest()->getCsrfToken()];
+    }
 }
