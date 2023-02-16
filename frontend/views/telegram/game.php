9efaae768557cee610ea70d0f266af79cdf799eb
garmayev <garmayev@yandex.ru>
2023-02-16 09:55:26 +0800

    Fix game

--- frontend/views/telegram/game.php
+++ frontend/views/telegram/game.php
@@ -17,6 +17,13 @@ use yii\web\View;
     <script src="//api-maps.yandex.ru/2.1/?apikey=0bb42c7c-0a9c-4df9-956a-20d4e56e2b6b&lang=ru_RU"
             type="text/javascript"></script>
     <script src="//code.jquery.com/jquery-3.6.3.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+
     <script>
         document.addEventListener("DOMContentLoaded", () => {
             $.ajax({
@@ -33,16 +40,18 @@ use yii\web\View;
             })
         })
     </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/webapp.css">
 </head>
 <body>
-<div class="slide">
-    <input type="text" name="User[login]" placeholder="<?= Yii::t('user', 'Login') ?>"/>
-    <input type="password" name="User[password]" placeholder="<?= Yii::t('user', 'Password') ?>"/>
-    <div class="log"></div>
+<div class="container-fluid">
+    <div class="row">
+        <input class="form-control" type="text" name="User[login]" placeholder="<?= Yii::t('user', 'Login') ?>"/>
+        <input class="form-control" type="password" name="User[password]"
+               placeholder="<?= Yii::t('user', 'Password') ?>"/>
+    </div>
 </div>
-<div class="slide">
-
-</div>
+<div class="log"></div>
 </body>
 </html>
