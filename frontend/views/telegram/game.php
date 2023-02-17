fc27f8e0942cecfe7b373045a21ce1e8da5fdb43
garmayev <garmayev@yandex.ru>
2023-02-17 12:17:21 +0800

    Datetimepicker

--- frontend/views/telegram/game.php
+++ frontend/views/telegram/game.php
@@ -12,34 +12,8 @@ use yii\web\View;
 ?>
 <html>
 <head>
-    <script src="//telegram.org/js/telegram-web-app.js"></script>
-    <script src="//kit.fontawesome.com/aa23fe1476.js"></script>
-    <script src="//api-maps.yandex.ru/2.1/?apikey=0bb42c7c-0a9c-4df9-956a-20d4e56e2b6b&lang=ru_RU"
-            type="text/javascript"></script>
-    <script src="//code.jquery.com/jquery-3.6.3.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossorigin="anonymous"></script>
-    <script type="module" src="/js/telegram-game.js"></script>
-    <script type="module">
-        import {Order, User} from "/js/telegram-game.js";
-
-        let tg = window.Telegram.WebApp;
-        let user = new User(document.querySelector("body > .container-fluid"), tg.initDataUnsafe.user ? tg.initDataUnsafe.user.id : "443353023");
-
-        user.on(User.EVENT_LOGGED, function (e) {
-            let orders = Order.get(this);
-            Order.buildTable(document.querySelector("body > .main"), orders);
-            console.log(orders);
-        }.bind(user))
-
-        user.init();
-        // console.log(user);
-    </script>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="https://natam03.ru/favicon.png"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/webapp.css">
@@ -58,7 +32,13 @@ use yii\web\View;
                 </button>
             </div>
             <div class="modal-body">
-                <input type="datetime-local" class="form-control">
+                <p>
+                    <input type="datetime-local" class="form-control">
+                </p>
+                <p>
+                    <input type="text" class="form-control">
+                </p>
+                <div id="map" style="min-height: 300px"></div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary"
@@ -68,5 +48,46 @@ use yii\web\View;
         </div>
     </div>
 </div>
+<script src="//telegram.org/js/telegram-web-app.js"></script>
+<script src="//kit.fontawesome.com/aa23fe1476.js"></script>
+<script src="//api-maps.yandex.ru/2.1/?apikey=0bb42c7c-0a9c-4df9-956a-20d4e56e2b6b&lang=ru_RU"
+        type="text/javascript"></script>
+<script src="//code.jquery.com/jquery-3.6.3.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+<script type="module" src="/js/telegram-game.js"></script>
+<script type="module">
+    import {Order, User} from "/js/telegram-game.js";
+
+    document.addEventListener("DOMContentLoaded", () => {
+        let tg = window.Telegram.WebApp, map = undefined,
+            user = new User(document.querySelector("body > .container-fluid"), tg.initDataUnsafe.user ? tg.initDataUnsafe.user.id : "443353023");
+
+        tg.expand();
+
+        user.on(User.EVENT_LOGGED, function (e) {
+            let orders = Order.get(this);
+            Order.buildTable(document.querySelector("body > .main"), orders);
+            // console.log(orders);
+        }.bind(user));
+        $('#exampleModal').on('show.bs.modal', () => {
+            if (map === undefined) {
+                map = new ymaps.Map("map", {
+                    center: [51.835501, 107.683123],
+                    zoom: 12,
+                    controls: [],
+                })
+            }
+            console.log("Modal open");
+        })
+
+        user.init();
+    })
+</script>
+
 </body>
 </html>
